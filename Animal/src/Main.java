6cbab95f90d350670f10619e5b3aecdfd2e3fb49
Derick Axel Lagunes Ramirez <dericklagunes@utez.edu.mx>
2025-06-09 12:13:36 -0600

    tercer commit

--- Animal/src/Main.java
+++ Animal/src/Main.java
@@ -1,6 +1,9 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello 3F!");
+        JOptionPane.showMessageDialog(null,"Hola como estas?");
     }
 }
